49c2c29915f35ec2c812bb0b4ad335005bc8e001
Petter Johansson <pjohansson@univ-pau.fr>
2021-07-22 09:47:16 +0200

    Remove unused convert function from `read` module

--- src/gmx_flow/data/read.py
+++ src/gmx_flow/data/read.py
@@ -51,33 +51,6 @@ def read_data(filename):
     return {l: grid[l] for l in _FIELDS}, info
 
 
-def convert_gmx_flow_1_to_2(data, info, width):
-    """Convert flow data from 'GMX_FLOW_1' to 'GMX_FLOW_2'.
-
-    This changes the field 'M' to represent the mass density instead of 
-    the total mass in the bin. Thus we also require the width of the system,
-    in order to calculate the bin volume.
-
-    If the `format` field in `info` is already 'GMX_FLOW_2' an unmodified 
-    copy of the original data is returned.
-
-    """
-
-    info = copy.deepcopy(info)
-    data = copy.deepcopy(data)
-
-    if info['format'] == 'GMX_FLOW_1':
-        (dx, dy) = info['spacing']
-        volume = dx * dy * width
-
-        if 'M' in data:
-            data['M'] /= volume
-
-        info['format'] = 'GMX_FLOW_2'
-
-    return data, info
-
-
 def _read_values(fp, num_values, fields):
     """Read the binary data in the given order."""
 
